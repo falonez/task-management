389f31babff316db0a5ef4bb7d3c3e4d972f40a8
Fathan Mubarok <50894040+falonez@users.noreply.github.com>
2023-11-08 11:56:25 +0700

    update input required and handle user create task for self

--- resources/views/Addtask.blade.php
+++ resources/views/Addtask.blade.php
@@ -1,25 +1,28 @@
 
 @extends('layouts.app')
 
+<!-- dumpt data user  -->
+
+
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-5">
             <div class="card">
             <form class="p-4" method="post" action="{{ route('task.create') }}">
                 @csrf
                 <div class="mb-2">
                     <label>Task Name</label>
-                    <input class="form-control" type="text" placeholder="Masukan Task" name="title" aria-label="default input example">
+                    <input class="form-control" type="text" required placeholder="Masukan Task" name="title" aria-label="default input example">
                 </div>
                 <div class="mb-2">
                     <label>Deskripsi</label>
-                    <input class="form-control" type="text" placeholder="Masukan Deskripsi" name="description" aria-label="default input example">
+                    <input class="form-control" type="text" required placeholder="Masukan Deskripsi" name="description" aria-label="default input example">
                 </div>
                 <div class="mb-2">
                     <label>Urgensi</label>
-                    <select class="form-select" name="urgency" aria-label="Default select example">
-                        <option selected disabled>Pick Urgency Task</option>
+                    <select class="form-select" name="urgency" aria-label="Default select example" required>
+                        <option selected disabled value="">Pick Urgency Task</option>
                         <option value="low">Low</option>
                         <option value="medium">Medium</option>
                         <option value="high">High</option>
@@ -27,23 +30,28 @@
                 </div>
                 <div class="mb-2">
                     <label>Durasi</label>
-                    <input class="form-control" type="number" placeholder="Masukan Durasi dalam satuan jam" name="duration" aria-label="default input example">
+                    <input class="form-control" type="number" required placeholder="Masukan Durasi dalam satuan jam" name="duration" aria-label="default input example">
                 </div>
                 <div class="mb-2">
                     <label>Batas Waktu</label>
-                    <input class="form-control" type="date" placeholder="Masukan Durasi dalam satuan jam" name="deadline" aria-label="default input example">
+                    <input class="form-control" type="date" required placeholder="Masukan Durasi dalam satuan jam" name="deadline" aria-label="default input example">
                 </div>
                 <div class="mb-2">
                     <label>User Assign</label>
-                    <select class="form-select" name="user_assign_task" aria-label="Default select example">
-                        <option selected disabled>Pick User</option>
+                    <select class="form-select" name="user_assign_task" aria-label="Default select example" required>
+                        <option selected disabled value="">Pick User</option>
                         @foreach ($user as $userr)
-                            <option value="{{ $userr->id }}">{{ $userr->name }}</option>
+                            @if($userr->id !== Auth::user()->id) {
+                                <option value="{{ $userr->id }}">{{ $userr->name }}</option>
+                            }
+                            @endif
                         @endforeach
                     </select>
                 </div>
                 <input type="hidden" value="{{ Auth::user()->id}}" name="user_create_task">
-                <button type="submit">Buat Tugas</button>
+                <div class="d-flex flex-row justify-content-end mb-3 mt-4">
+                    <button type="submit" class="btn btn-sm btn-primary">Buat Tugas</button>
+                </div>  
             </form>
             </div>
         </div>
